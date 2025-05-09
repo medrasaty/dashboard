957f24d5e3ca0730a6b863946442d50bc87d8cc3
ahmedshayea <ahmedshayeasulaimany@gmail.com>
2025-05-09 10:11:04 +0300

    teacher more info now display list of human readable subjects instead of raw ids

--- src/features/users/components/UserMoreInfoCard.tsx
+++ src/features/users/components/UserMoreInfoCard.tsx
@@ -1,20 +1,14 @@
-"use client";
 import InfoCard from "./InfoCard";
-import {
-  Dialog,
-  DialogContent,
-  DialogTitle,
-  Grid2,
-  Typography,
-} from "@mui/material";
+import { Grid2 } from "@mui/material";
 import { useUserDetails } from "../providers";
 import { StudentMoreType, TeacherMoreType } from "../types";
 import InfoItem, { InfoItemProps } from "@/components/InfoItem";
 import useOpen from "@/hooks/useOpen";
 import { StyledDialog, StyledDialogTitle } from "@/components/styled/Dialog";
 import UpdateStudentMoreForm from "./forms/UpdateStudentMoreForm";
-import { useMemo } from "react";
+import React, { JSX, useMemo } from "react";
 import { UpdateTeacherMoreForm2 } from "./forms/UpdateTeacherMoreForm";
+import { useSubjects } from "../queries";
 
 /**
  * Dynamic card based on user type
@@ -22,12 +16,49 @@ import { UpdateTeacherMoreForm2 } from "./forms/UpdateTeacherMoreForm";
 export default function UserMoreInfoCard() {
   const user = useUserDetails();
 
+  // different card for different users.
   const MoreCard = useMemo(
     () => (user.type == "STUDENT" ? StudentMoreCard : TeacherMoreCard),
     [user.type]
   );
 
   return <MoreCard more={user.more} />;
+}
+
+function MoreCardBase({
+  title,
+  infoItems,
+  dialogTitle,
+  dialogContent,
+  onEdit,
+  open,
+  hide,
+}: {
+  title: string;
+  infoItems: InfoItemProps[];
+  dialogTitle: string;
+  dialogContent: JSX.Element;
+  onEdit: () => void;
+  open: boolean;
+  hide: () => void;
+}) {
+  return (
+    <>
+      <InfoCard title={title} onEdit={onEdit}>
+        <Grid2 container spacing={3}>
+          {infoItems.map((info, index) => (
+            <Grid2 size={{ xs: 12, sm: 6 }} key={index}>
+              <InfoItem {...info} />
+            </Grid2>
+          ))}
+        </Grid2>
+      </InfoCard>
+      <StyledDialog open={open} onClose={hide}>
+        <StyledDialogTitle>{dialogTitle}</StyledDialogTitle>
+        {dialogContent}
+      </StyledDialog>
+    </>
+  );
 }
 
 function StudentMoreCard({ more: studentmore }: { more: StudentMoreType }) {
@@ -41,29 +72,36 @@ function StudentMoreCard({ more: studentmore }: { more: StudentMoreType }) {
   ] satisfies InfoItemProps[];
 
   return (
-    <>
-      <InfoCard title="More" onEdit={ShowEditDialog}>
-        <Grid2 container spacing={3}>
-          {infoItems.map((info, index) => (
-            <Grid2 size={{ xs: 12, sm: 6 }} key={index}>
-              <InfoItem {...info} />
-            </Grid2>
-          ))}
-        </Grid2>
-      </InfoCard>
-      <StyledDialog open={open} onClose={hide}>
-        <StyledDialogTitle>Edit Student More</StyledDialogTitle>
+    <MoreCardBase
+      title="More"
+      infoItems={infoItems}
+      dialogTitle="Edit Student More"
+      dialogContent={
         <UpdateStudentMoreForm
           initialGrade={studentmore.grade}
           onClose={hide}
         />
-      </StyledDialog>
-    </>
+      }
+      onEdit={ShowEditDialog}
+      open={open}
+      hide={hide}
+    />
   );
 }
 
 function TeacherMoreCard({ more: teachermore }: { more: TeacherMoreType }) {
   const { open, show, hide } = useOpen();
+
+  const { data: subjects, isPending } = useSubjects();
+
+  // get the subject names from the subjects ids array
+  const subjectNames = subjects
+    ?.filter((subject) => {
+      return teachermore.subjects?.includes(subject.id);
+    })
+    .map((subject) => subject.name)
+    .join(", ");
+
   const infoItems: InfoItemProps[] = useMemo(
     () => [
       {
@@ -72,29 +110,21 @@ function TeacherMoreCard({ more: teachermore }: { more: TeacherMoreType }) {
       },
       {
         label: "Subjects",
-        value: teachermore.subjects?.toString(),
+        value: isPending ? "Loading..." : subjectNames,
       },
     ],
-    [teachermore]
+    [teachermore, isPending, subjectNames]
   );
 
   return (
-    <>
-      <InfoCard title="More" onEdit={show}>
-        <Grid2 container spacing={3}>
-          {infoItems.map((info, index) => (
-            <Grid2 size={{ xs: 12, sm: 6 }} key={index}>
-              <InfoItem {...info} />
-            </Grid2>
-          ))}
-        </Grid2>
-      </InfoCard>
-      <StyledDialog open={open} onClose={hide}>
-        <DialogTitle>Edit more</DialogTitle>
-        <DialogContent>
-          <UpdateTeacherMoreForm2 onClose={hide} />
-        </DialogContent>
-      </StyledDialog>
-    </>
+    <MoreCardBase
+      title="More"
+      infoItems={infoItems}
+      dialogTitle="Edit more"
+      dialogContent={<UpdateTeacherMoreForm2 onClose={hide} />}
+      onEdit={show}
+      open={open}
+      hide={hide}
+    />
   );
 }
